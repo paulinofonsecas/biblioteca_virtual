a913f7465b369eb5daa6a21bcd3edea0257079f9
Paulino Fonseca <paulinofonsecass@gmail.com>
2024-04-14 19:20:21 +0100

    init project

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.bilioteca_virtual
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:bilioteca_virtual/app/app.dart';
+import 'package:bilioteca_virtual/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
