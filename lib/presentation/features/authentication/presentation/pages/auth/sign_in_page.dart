f3b67ec6bf0c5c50558e2fb9301a6c9c52cecc5b
Paulino Fonseca <paulinofonsecass@gmail.com>
2024-04-19 14:06:18 +0100

    Altera o svg da tela de login

--- lib/presentation/features/authentication/presentation/pages/auth/sign_in_page.dart
+++ lib/presentation/features/authentication/presentation/pages/auth/sign_in_page.dart
@@ -1,7 +1,6 @@
 import 'package:bilioteca_virtual/presentation/features/authentication/presentation/widgets/auth/sign_in_form.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_gutter/flutter_gutter.dart';
-import 'package:flutter_svg/flutter_svg.dart';
 
 class SignIn extends StatefulWidget {
   const SignIn({super.key});
@@ -17,10 +16,10 @@ class _SignInState extends State<SignIn> {
       child: Scaffold(
         body: ListView(
           children: [
-            Padding(
-              padding: const EdgeInsets.all(20),
-              child: SvgPicture.asset(
-                'svgs/man-reading-book.svg',
+            const Padding(
+              padding: EdgeInsets.all(20),
+              child: Image(
+                image: AssetImage('assets/home_page_background.gif'),
                 height: 200,
                 width: 200,
               ),
