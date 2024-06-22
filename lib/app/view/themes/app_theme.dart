1fd03edaec5e22f10af5248349bb1fd02338ca6e
Paulino Fonseca <paulinofonsecass@gmail.com>
2024-06-22 21:56:54 +0100

    ajusta o tema

--- lib/app/view/themes/app_theme.dart
+++ lib/app/view/themes/app_theme.dart
@@ -1,9 +1,18 @@
 import 'package:bilioteca_virtual/app/cubit/app_brightness_cubit.dart';
+import 'package:bilioteca_virtual/core/util/constants.dart';
 import 'package:flutter/material.dart';
 
 ThemeData appTheme(AppBrightnessState appBrightnessState) => ThemeData(
       colorScheme: ColorScheme.fromSeed(
         seedColor: Colors.green,
         brightness: appBrightnessState.brightness,
+      ),
+      inputDecorationTheme: InputDecorationTheme(
+        contentPadding: const EdgeInsets.symmetric(
+          horizontal: kDefaultPadding / 2,
+        ),
+        border: UnderlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
       ),
     );
--- lib/presentation/features/global_search/widgets/searched_item.dart
+++ lib/presentation/features/global_search/widgets/searched_item.dart
@@ -1,8 +1,8 @@
 // ignore_for_file: public_member_api_docs, sort_constructors_first
 import 'package:bilioteca_virtual/domain/entities/book.dart';
+import 'package:bilioteca_virtual/presentation/admin/features/admin_view_book/view/admin_view_book_page.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_modular/flutter_modular.dart';
 
 class SearchedItem extends StatelessWidget {
   const SearchedItem({
@@ -17,15 +17,9 @@ class SearchedItem extends StatelessWidget {
     return ListTile(
       title: Text(book.title),
       onTap: () {
-        Modular.to.pushNamed(
-          '/admin/view-book',
-          arguments: {
-            'bookId': book.id,
-            'parent': '/admin/home-page',
-          },
-        );
+        AdminViewBookPage.toScreen(book.id);
       },
-      subtitle: Text(book.resumo ?? 'Sem resumo'),
+      subtitle: Text(book.resumo ?? 'Sem resumo', maxLines: 2),
       leading: ClipRRect(
         borderRadius: BorderRadius.circular(8),
         child: CachedNetworkImage(
