09dc14735478430d195aeb0c2edd4c2bf9167d9f
Paulino Fonseca <paulinofonsecass@gmail.com>
2024-07-13 05:11:11 +0100

    validar comprovante pelo cubit

---
+++ lib/presentation/admin/features/admin_view_book/cubit/validar_compra_cubit.dart
@@ -0,0 +1,61 @@
+import 'package:bilioteca_virtual/core/dependency/get_it.dart';
+import 'package:bilioteca_virtual/domain/entities/book.dart';
+import 'package:bilioteca_virtual/domain/entities/payment.dart';
+import 'package:bilioteca_virtual/domain/use_cases/payment_use_cases.dart';
+import 'package:bloc/bloc.dart';
+import 'package:equatable/equatable.dart';
+import 'package:uuid/uuid.dart';
+import 'package:verify_payment/models/response_model.dart';
+
+part 'validar_compra_state.dart';
+
+class ValidarCompraCubit extends Cubit<ValidarCompraState> {
+  ValidarCompraCubit() : super(ValidarCompraInitial()) {
+    _paymentUseCases = getIt<IPaymentUseCases>();
+  }
+
+  late final IPaymentUseCases _paymentUseCases;
+
+  Future<void> validarLivro(
+    BookModel book,
+    VerifyPaymentSuccess? paymentValidator,
+  ) async {
+    emit(ValidarCompraLoading());
+
+    if (paymentValidator != null) {
+      if (book.preco.valor == paymentValidator.dINHEIRO!.toDouble()) {
+        final payment = Payment(
+          id: const Uuid().v4(),
+          bookId: book.id,
+          amount: paymentValidator.dINHEIRO!.toDouble(),
+          currency: 'Kz',
+          date: DateTime.parse(paymentValidator.dATA!.data!),
+          paymentMethod: 'Transferencia',
+          transactionId: paymentValidator.tRANSACAO!.toString(),
+        );
+
+        await _paymentUseCases.processPayment(payment).then(
+          (value) {
+            emit(ValidarCompraPaymentSuccess(payment));
+          },
+        );
+      } else {
+        if (paymentValidator.dINHEIRO! > book.preco.valor) {
+          emit(
+            const ValidarCompraPaymentFailure(
+              'O Valor é superior ao do livro',
+            ),
+          );
+        } else {
+          emit(
+            const ValidarCompraPaymentFailure(
+              'O Valor é inferior ao do livro',
+            ),
+          );
+        }
+      }
+    } else {
+      emit(const ValidarCompraPaymentFailure('Pagamento inválido'));
+    }
+  }
+}
---
+++ lib/presentation/admin/features/admin_view_book/cubit/validar_compra_state.dart
@@ -0,0 +1,49 @@
+part of 'validar_compra_cubit.dart';
+
+sealed class ValidarCompraState extends Equatable {
+  const ValidarCompraState();
+
+  @override
+  List<Object> get props => [];
+}
+
+final class ValidarCompraInitial extends ValidarCompraState {}
+
+
+final class ValidarCompraPaymentSuccess extends ValidarCompraState {
+  const ValidarCompraPaymentSuccess(this.paymentDetails);
+
+  final Payment paymentDetails;
+
+  @override
+  List<Object> get props => [paymentDetails];
+}
+
+final class ValidarCompraPaymentFailure extends ValidarCompraState {
+  const ValidarCompraPaymentFailure(this.message);
+
+  final String message;
+
+  @override
+  List<Object> get props => [message];
+}
+
+final class ValidarCompraLoading extends ValidarCompraState {}
+
+final class ValidarCompraError extends ValidarCompraState {
+  const ValidarCompraError(this.message);
+
+  final String message;
+
+  @override
+  List<Object> get props => [message];
+}
+
+final class ValidarCompraLoaded extends ValidarCompraState {
+  const ValidarCompraLoaded(this.book);
+
+  final BookModel book;
+
+  @override
+  List<Object> get props => [book];
+}
