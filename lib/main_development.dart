4d5fd08139dcdad4ae8cbaf3083812d5d87edf07
Paulino Fonseca <paulinofonsecass@gmail.com>
2025-01-30 11:55:55 +0100

    feat: update icons

--- lib/main_development.dart
+++ lib/main_development.dart
@@ -1,4 +1,4 @@
-// import 'dart:io';
+// import 'dart:io' main_production;
 
 import 'dart:async';
 
--- lib/main_production.dart
+++ lib/main_production.dart
@@ -1,18 +1,39 @@
+// import 'dart:io' main_production;
+
+import 'dart:async';
+
 import 'package:bilioteca_virtual/app/app.dart';
+import 'package:bilioteca_virtual/app/modules/app_module.dart';
 import 'package:bilioteca_virtual/bootstrap.dart';
 import 'package:bilioteca_virtual/core/dependency/get_it.dart';
 import 'package:bilioteca_virtual/firebase_options.dart';
+// import 'package:device_preview/device_preview.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_modular/flutter_modular.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  // unawaited(MobileAds.instance.initialize());
 
   await Firebase.initializeApp(
     options: DefaultFirebaseOptions.currentPlatform,
   );
 
-  await setupDependencies();
+  await setupCoreDependencies();
 
-  await bootstrap(() => const App());
+  unawaited(
+    bootstrap(
+      () {
+        // if (Platform.isAndroid || Platform.isIOS) {
+        //   return ModularApp(module: AppModule(), child: const App());
+        // }
+
+        return ModularApp(
+          module: AppModule(),
+          child: const App(),
+        );
+      },
+    ),
+  );
 }
